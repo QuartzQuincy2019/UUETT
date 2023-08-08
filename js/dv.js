417b402a9523a28050f7e4b9c0ac850f5548b1b3
Quincy K <79129150+QuartzQuincy2019@users.noreply.github.com>
2023-08-08 14:58:34 +0800

    Update dv.js
    v4.2.1

--- js/dv.js
+++ js/dv.js
@@ -5,7 +5,7 @@
 /**
  * 当前版本。只有贡献者对此有解释权。
  */
-var _VERSION = "v4.0.0";
+var _VERSION = "v4.2.1";
 
 /**
  * 最大皮肤ID。
@@ -32,16 +32,17 @@ var __SKIN_HINTS = [
     "Turn On the Light"
 ];
 
-var __DEFAULT_FONT_TYPING = "Consolas";
-var __DEFAULT_FONT_OTHER = 'Times New Roman';
+//默认字体设置
+var __DEFAULT_FONT_TYPING = "Consolas";//打字区域显示字体
+var __DEFAULT_FONT_OTHER = 'Times New Roman';//其他区域显示字体
 
 //功能键设置
-var __FK_LAUNCH_TASK = "Control";
-var __FK_MODE_SWITCH = "Tab";
-var __FK_TIMER_RESTART = "Alt";
-var __FK_CLEAR = "F1";
-var __FK_MOVE_SKIN = "F10";
-var __FK_TIMER_TOGGLE = "F12";
-var __FK_DEFAULT_FONT_SIZE = "F9";
-var __FK_INCREASE_FONT_SIZE = "PageUp";
-var __FK_DECREASE_FONT_SIZE = "PageDown";
+var __FK_LAUNCH_TASK = "Control";//刷新任务
+var __FK_MODE_SWITCH = "Tab";//输入模式切换
+var __FK_TIMER_RESTART = "Alt";//重新计时
+var __FK_CLEAR = "F1";//清除输入
+var __FK_MOVE_SKIN = "F10";//切换皮肤
+var __FK_TIMER_TOGGLE = "F12";//暂停或开始计时
+var __FK_DEFAULT_FONT_SIZE = "F9";//默认字号
+var __FK_INCREASE_FONT_SIZE = "PageUp";//增大字号
+var __FK_DECREASE_FONT_SIZE = "PageDown";//减小字号
